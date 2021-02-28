53c1e22d2bec1d119a2b9921d0e7113e9615e483
thedocf <49178382+thedocf@users.noreply.github.com>
2021-02-28 19:07:11 +0100

    notification_v0

--- flutter_app/workspace/lib/main.dart
+++ flutter_app/workspace/lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+import 'package:flutter_sms/flutter_sms.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+void _sendSMS(String message, List<String> recipents) async {
+  String _result = await sendSMS(message: message, recipients: recipents)
+      .catchError((onError) {
+    print(onError);
+  });
+  print(_result);
+}
+
+void sendit() {
+  String message = "This is a test message!";
+  List<String> recipents = ["+21651775991"];
+
+  _sendSMS(message, recipents);
+}
+
+sendMail() async {
+  String username = 'fitfoot.service@gmail.com';
+  String password = 'fitfoot123';
+
+  final smtpServer = gmail(username, password);
+
+  final message = Message()
+    ..from = Address(username, 'FitFoot')
+    ..recipients.add('firas.selmen@esprit.tn')
+    ..subject = 'FitFoot Notification ${DateTime.now()}'
+    ..html =
+        "<h1>Patient : Flen Ben Foulen</h1>\n<p>Hey! Here's some patient details</p>";
+
+  try {
+    final sendReport = await send(message, smtpServer);
+    print('Message sent: ' + sendReport.toString());
+  } on MailerException catch (e) {
+    print('Message not sent.');
+    for (var p in e.problems) {
+      print('Problem: ${p.code}: ${p.msg}');
+    }
+  }
+  sendit();
 }
 
 class MyApp extends StatelessWidget {
@@ -97,21 +140,19 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            FlatButton(
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0)),
+                onPressed: sendMail,
+                child: Text(
+                  "Notifier Medecin",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                )),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
