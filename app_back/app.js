1b0a9bfea847f25d589852b06171e0d4118f83d6
TahaJemmali <mohamedtaha.jammali@esprit.tn>
2021-02-28 11:26:31 +0100

    first commit

---
+++ app_back/app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const url = require('./config/databaseUrl.json').DataBaseUrl;
+
+var app = express();
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology',true);
+mongoose.set('useCreateIndex', true);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(express.json({limit: '50mb', extended: true}));
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+//Connection to mongoDB
+mongoose.connect(url)
+.then((data) => console.log("database connected"))
+.catch((err) => console.log(err))
+
+
+module.exports = app;
---
+++ flutter_app/README.md
@@ -0,0 +1,5 @@
+when switching from the local workspace to the remote one we have to execute this following commands :
+flutter pub get
+flutter packages get
+
+PS: And when switching back to the local workspace directory we have to execute the same commands
