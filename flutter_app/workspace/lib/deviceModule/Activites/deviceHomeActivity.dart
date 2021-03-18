150cdf2e41617aa44b68469899b8d34a00174336
tahaJemmali <49129221+tahaJemmali@users.noreply.github.com>
2021-03-18 22:41:44 +0100

    communication with the device

--- flutter_app/workspace/lib/deviceModule/Activites/deviceHomeActivity.dart
+++ flutter_app/workspace/lib/deviceModule/Activites/deviceHomeActivity.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
@@ -9,11 +12,42 @@ class DeviceHomeActivity extends StatefulWidget {
 }
 
 class _DeviceHomeActivityState extends State<DeviceHomeActivity> {
+  BluetoothConnection connection;
+  bool isConnecting = true;
+  bool isDisconnecting = true;
+
+  connect(String address) async {
+    if (isConnecting) {
+      BluetoothConnection.toAddress(widget.device.address).then((_connection) {
+        print('Connected to the device');
+        connection = _connection;
+        connection.input.listen((Uint8List data) {
+          print(ascii.decode(data));
+        });
+        setState(() {
+          isConnecting = false;
+          isDisconnecting = false;
+        });
+      });
+      print('Connected to the device');
+    } else {
+      print('already connected to the device');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    connect(widget.device.address);
     return Scaffold(
       appBar: AppBar(
         title: Text("Connected to " + widget.device.name),
+      ),
+      body: FlatButton(
+        onPressed: () /*async*/ {
+          connection.output.add(utf8.encode("1" + "\r\n"));
+          /* await*/ connection.output.allSent;
+        },
+        child: Text("hello"),
       ),
     );
   }
